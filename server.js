4efac7d3ed23d91a812b84a3e517cc6f29073011
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2026-03-27 18:50:35 +0530

    feat: implement room-based chat using Socket.IO rooms

--- server.js
+++ server.js
@@ -12,27 +12,36 @@ const io = new Server(server);
 io.on("connection", (socket) => {
   console.log("User connected", socket.id);
 
-  socket.on("user-joined", (username) => {
+  socket.on("join-room", ({ username, room }) => {
     socket.username = username;
-    socket.broadcast.emit("user-joined", `${username} joined the chat`);
+    socket.room = room;
+
+    socket.join(room);
+    socket.emit("user-joined", `You joined ${room}`);
+    // socket.broadcast.emit("user-joined", `${username} joined the chat`);
+    socket.broadcast.to(room).emit("user-joined", `${username} joined ${room}`);
   });
   socket.on("user-message", (data) => {
-    io.emit("message", data);
+    io.to(socket.room).emit("message", data);
+    // io.emit("message", data);
     // console.log("A new User Message", message);
   });
 
   socket.on("disconnect", () => {
-    if (socket.username) {
-      socket.broadcast.emit("user-left", `${socket.username} left the chat`);
+    if (socket.username && socket.room) {
+      // socket.broadcast.emit("user-left", `${socket.username} left the chat`);
+      socket.broadcast
+        .to(socket.room)
+        .emit("user-left", `${socket.username} left ${socket.room}`);
     }
   });
 });
 
 app.use(express.static(path.resolve("./public")));
 
-app.get("/", (req, res) => {
-  return res.sendFile("/public/index.html");
-});
+// app.get("/", (req, res) => {
+//   return res.sendFile("/public/index.html");
+// });
 
 server.listen(PORT, () =>
   console.log(`Server running on port http://localhost:${PORT}`),
