58ea0914e02d088f2aa7dd20323eda66dd2a04b8
Sonal Ghinaiya <ghinaiyasonal1308@gmail.com>
2026-05-19 10:23:08 +0530

    feat(auth): implement user registration and login functionality with JWT authentication

--- server/server.js
+++ server/server.js
@@ -2,10 +2,13 @@ import http from "http";
 import express from "express";
 import cors from "cors";
 import { Server } from "socket.io";
+import authRoutes from "./routes/authRoutes.js"
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const server = http.createServer(app);
-const PORT = 9000;
+const PORT = process.env.PORT || 5000;
+const url = process.env.MONGODB_URI;
 const io = new Server(server, {
   cors: {
     origin: ["http://localhost:5173", "https://vibely-web-chat.vercel.app"],
@@ -50,6 +53,10 @@ io.on("connection", (socket) => {
     }
   });
 });
+
+app.use(express.json());
+app.use("/api/auth", authRoutes)
+connectDB(url).then(() => console.log("MongoDB Connected!"));
 
 // app.use(express.static(path.resolve("./public")));
 
